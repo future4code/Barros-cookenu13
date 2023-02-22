edcd00b7aa1d17ee4d969fbb154e7d0f93d8e209
Ricardo_Barros <ricardo_teleco@hotmail.com>
2023-02-21 22:52:51 -0300

    Esse commit adiciona o token ao endpoint signup

--- src/router/UserRouter.ts
+++
@@ -1,9 +0,0 @@
-/* import  express  from "express"
-import { SignupController } from "../controller/SignupController"
-
-export const userRouter = express.Router()
-const userController = new SignupController()
-
-userRouter.post("/signup", userController.signup)
-
- */
